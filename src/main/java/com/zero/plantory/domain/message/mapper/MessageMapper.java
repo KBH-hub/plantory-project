0561a8900d25b47df9ca563d7c83870e21804258
KBH <galaxysgoo418@gmail.com>
2025-11-21 01:04:01 +0900

    add: message mapper, vo, dao file

--- src/main/java/com/zero/plantory/domain/message/mapper/MessageMapper.java
+++ src/main/java/com/zero/plantory/domain/message/mapper/MessageMapper.java
@@ -1,8 +1,16 @@
 package com.zero.plantory.domain.message.mapper;
 
+import com.zero.plantory.domain.message.vo.MessageVO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
-public class MessageMapper {
-    //test
+public interface MessageMapper {
+    List<MessageVO> selectMessages(Long memberId, String boxType, String targetType, String title);
+    int updateReadFlag(Long messageId);
+    int deleteMessages(List<Long> messageIds);
+    MessageVO selectMessageWriteInfo(Long senderId, String targetType, Long targetId);
+    int insertMessage(MessageVO message);
+    MessageVO selectMessageDetail(Long senderId, String targetType, Long targetId);
 }
---
+++ src/main/java/com/zero/plantory/domain/message/vo/MessageVO.java
@@ -0,0 +1,25 @@
+package com.zero.plantory.domain.message.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageVO {
+    private Long messageId;
+    private Long senderId;
+    private String senderNickname;
+    private Long receiverId;
+    private String receiverNickname;
+    private String title;
+    private String content;
+    private String targetType;
+    private Long targetId;
+    private Date createdAt;
+    private Date readFlag;
+    private Date delFlag;
+}
