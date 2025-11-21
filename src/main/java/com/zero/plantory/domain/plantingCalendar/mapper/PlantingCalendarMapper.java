f598edcb44cd45f44995ee9650e918dc5d09b45e
kbh_kosta <galaxysgoo418@gmail.com>
2025-11-21 19:49:16 +0900

    add: add PlantingCalendar

--- src/main/java/com/zero/plantory/domain/plantingCalendar/mapper/PlantingCalendarMapper.java
+++ src/main/java/com/zero/plantory/domain/plantingCalendar/mapper/PlantingCalendarMapper.java
@@ -4,10 +4,15 @@ import com.zero.plantory.domain.plantingCalendar.vo.PlantingCalendarVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
+import java.util.List;
+
 @Mapper
 public interface PlantingCalendarMapper {
     int insertWatering(@Param("myplantId") Long myplantId);
     int updatePlantWateringCheck(@Param("wateringId") Long wateringId);
     int updateMyPlantWatering(@Param("myplantId") Long myplantId);
     int deletePlantWatering(@Param("myplantId") Long myplantId);
+    List<PlantingCalendarVO> selectWateringCalendar(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
+    List<PlantingCalendarVO> selectDiaryCalendar(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }
--- src/main/java/com/zero/plantory/domain/plantingCalendar/vo/PlantingCalendarVO.java
+++ src/main/java/com/zero/plantory/domain/plantingCalendar/vo/PlantingCalendarVO.java
@@ -16,4 +16,8 @@ public class PlantingCalendarVO {
     private Long myplantId;
     private Date dateAt;
     private Date checkFlag;
+    private Long id;           // diary_id / watering_id
+    private String myplantName;// 식물 이름
+    private String content;    // 다이어리 메모(물주기는 null)
+    private String type;
 }
