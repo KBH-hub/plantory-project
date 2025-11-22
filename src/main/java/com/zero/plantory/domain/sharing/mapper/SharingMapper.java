a4965f3234763d9d28c3471d443d2d2cbb676422
minse00 <tjdlsj@naver.com>
2025-11-22 18:03:04 +0900

    remove: remove imageDAO from sharing

--- src/main/java/com/zero/plantory/domain/sharing/mapper/SharingMapper.java
+++ src/main/java/com/zero/plantory/domain/sharing/mapper/SharingMapper.java
@@ -2,8 +2,6 @@ package com.zero.plantory.domain.sharing.mapper;
 
 import com.zero.plantory.domain.sharing.vo.*;
 import com.zero.plantory.global.vo.CommentVO;
-import com.zero.plantory.global.vo.ImageTargetType;
-import com.zero.plantory.global.vo.ImageVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import com.zero.plantory.global.vo.SharingVO;
@@ -36,8 +34,6 @@ public interface SharingMapper {
     /**update sharing*/
     int countMySharing(@Param("sharingId") Long sharingId, @Param("memberId") Long memberId);
     int updateSharing(SharingVO vo);
-    int deleteSharingImage(@Param("targetType") ImageTargetType targetType, @Param("targetId") Long targetId, @Param("imageId") Long imageId);
-    int insertSharingImage(ImageVO vo);
 
     int deleteSharing(@Param("sharingId") Long sharingId);
 
