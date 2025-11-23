bb6750362ca57dd97d8312f36411270ab9b369a3
KBH <galaxysgoo418@gmail.com>
2025-11-23 20:23:22 +0900

    add: add message service file

---
+++ src/test/java/com/zero/plantory/domain/message/service/MessageServiceTest.java
@@ -0,0 +1,111 @@
+package com.zero.plantory.domain.message.service;
+
+import com.zero.plantory.domain.message.vo.SelectMessageListVO;
+import com.zero.plantory.domain.message.vo.SelectMessageSearchVO;
+import com.zero.plantory.global.vo.MessageTargetType;
+import com.zero.plantory.global.vo.MessageVO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+class MessageServiceTest {
+
+    @Autowired
+    private MessageService messageService;
+
+    @Test
+    @DisplayName("쪽지 리스트 조회")
+    void getMessageListTest() {
+        SelectMessageSearchVO vo = SelectMessageSearchVO.builder()
+                .memberId(2L)
+                .boxType("SENT")
+                .targetType("SHARING")
+                .title(null)
+                .limit(10)
+                .offset(0)
+                .build();
+
+        List<SelectMessageListVO> result = messageService.getMessageList(vo);
+
+        log.info("result={}", result);
+
+    }
+
+    @Test
+    @DisplayName("메시지 삭제 처리")
+    void removeMessage() {
+        List<Long> messageIds = List.of(1L, 2L);
+        Long removerId = 1L;
+
+        int result = messageService.removeMessages(messageIds, removerId);
+
+        log.info(String.valueOf(result));
+    }
+
+    @Test
+    @DisplayName("메시지 전송 시 필요 정보 조회")
+    void findMessageWriteInfo() {
+        Long senderId = 11L; // 쪽지 보내는 사람
+        String targetType = "QUESTION"; // 나눔 글 기준
+        Long targetId = 3L;
+
+        MessageVO result = messageService.findMessageWriteInfo(senderId, targetType, targetId);
+
+        log.info(String.valueOf(result));
+    }
+
+    @Test
+    @DisplayName("메시지 전송 실패 (제목 누락)처리")
+    void registerFailByTitleMessage() {
+        MessageVO vo = new MessageVO().builder()
+                .senderId(3L)
+                .receiverId(8L)
+                .title("")
+                .content("테스트 쪽지 내용입니다.")
+                .targetType(MessageTargetType.SHARING)
+                .targetId(13L)
+                .build();
+
+        int result = messageService.registerMessage(vo);
+
+        log.info("inserted rows = " + result);
+    }
+
+    @Test
+    @DisplayName("메시지 전송 실패 (내용 누락)처리")
+    void registerFailByContentMessage() {
+        MessageVO vo = new MessageVO().builder()
+                .senderId(3L)
+                .receiverId(8L)
+                .title("테스트 제목입니다.")
+                .content("")
+                .targetType(MessageTargetType.SHARING)
+                .targetId(13L)
+                .build();
+
+        int result = messageService.registerMessage(vo);
+
+        log.info("inserted rows = " + result);
+    }
+
+    @Test
+    @DisplayName("메시지 상세 정보 조회")
+    void findMessageDetail() {
+        Long messageId = 1L;
+        Long viewerId = 1L;
+
+        MessageVO result = messageService.findMessageDetail(messageId, viewerId);
+
+        log.info(String.valueOf(result));
+    }
+
+}
