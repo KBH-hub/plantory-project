c8e8ba19ae6e4ac24572e00a5b41e8116ccb2a8e
KBH <galaxysgoo418@gmail.com>
2025-11-24 01:47:33 +0900

    feat: add plant calender service, test

---
+++ src/main/java/com/zero/plantory/domain/plantingCalendar/service/PlantingCalenderServiceImpl.java
@@ -0,0 +1,133 @@
+package com.zero.plantory.domain.plantingCalendar.service;
+
+import com.zero.plantory.domain.image.ImageMapper;
+import com.zero.plantory.domain.plantingCalendar.mapper.PlantingCalendarMapper;
+import com.zero.plantory.domain.plantingCalendar.vo.PlantingCalendarVO;
+import com.zero.plantory.domain.plantingCalendar.vo.selectMyPlantDiaryVO;
+import com.zero.plantory.domain.report.mapper.ReportMapper;
+import com.zero.plantory.global.utils.StorageUploader;
+import com.zero.plantory.global.vo.DiaryVO;
+import com.zero.plantory.global.vo.ImageTargetType;
+import com.zero.plantory.global.vo.ImageVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class PlantingCalenderServiceImpl implements PlantingCalenderService {
+    private final PlantingCalendarMapper plantingCalendarMapper;
+    private final ImageMapper imageMapper;
+    private final StorageUploader storageUploader;
+
+    @Override
+    public int registerWatering(Long myplantId) {
+        return plantingCalendarMapper.insertWatering(myplantId);
+    }
+
+    @Override
+    public int updatePlantWateringCheck(Long wateringId) {
+        return plantingCalendarMapper.updatePlantWateringCheck(wateringId);
+    }
+
+    @Override
+    @Transactional
+    public int removePlantWatering(Long myplantId, Long removerId) {
+        List<selectMyPlantDiaryVO> list = plantingCalendarMapper.selectMyPlant(removerId);
+        for (selectMyPlantDiaryVO myPlant : list) {
+            if (Objects.equals(myPlant.getMyplantId(), myplantId)) {
+                plantingCalendarMapper.updateMyPlantWatering(myplantId);
+                plantingCalendarMapper.deletePlantWatering(myplantId);
+            }
+            return 1;
+        }
+        throw new IllegalStateException("물주기 삭제 실패(식물 소유자 미일치)");
+    }
+
+    @Override
+    public List<PlantingCalendarVO> getWateringCalendar(Date startDate, Date endDate) {
+        return plantingCalendarMapper.selectWateringCalendar(startDate, endDate);
+    }
+
+    @Override
+    public List<PlantingCalendarVO> getDiaryCalendar(Date startDate, Date endDate) {
+        return plantingCalendarMapper.selectDiaryCalendar(startDate, endDate);
+    }
+
+    @Override
+    public DiaryVO findDiaryUpdateInfo(Long diaryId) {
+        return plantingCalendarMapper.selectDiaryUpdateInfo(diaryId);
+    }
+
+    @Override
+    public List<ImageVO> findDiaryUpdateImageInfo(Long diaryId) {
+        return imageMapper.selectImagesByTarget(ImageTargetType.DIARY, diaryId);
+    }
+
+    @Override
+    @Transactional
+    public int updateDiary(DiaryVO diaryVO, List<ImageVO> delImgList, List<MultipartFile> files, Long memberId) throws IOException {
+        int result = 0;
+        result += plantingCalendarMapper.updateDiary(diaryVO);
+
+        for (ImageVO image : delImgList) {
+            result += imageMapper.softDeleteImagesByTarget(ImageTargetType.DIARY, image.getImageId());
+        }
+
+        for(MultipartFile file:files){
+            String url = storageUploader.uploadFile(file);
+
+            ImageVO image = ImageVO.builder()
+                    .memberId(memberId)
+                    .targetType(ImageTargetType.DIARY)
+                    .targetId(diaryVO.getDiaryId())
+                    .fileUrl(url)
+                    .fileName(file.getOriginalFilename())
+                    .build();
+            result += imageMapper.insertImage(image);
+
+        }
+
+        if(result == delImgList.size()+files.size()+1){
+            return result;
+        }
+        throw new IllegalStateException("관찰일지 수정 실패(업데이트 누락)");
+    }
+
+    @Override
+    public List<selectMyPlantDiaryVO> getMyPlant(Long memberId) {
+        return plantingCalendarMapper.selectMyPlant(memberId);
+    }
+
+    @Override
+    @Transactional
+    public int registerDiary(DiaryVO diaryVO, List<MultipartFile> files, Long memberId) throws IOException {
+        int result = 0;
+        result += plantingCalendarMapper.insertDiary(diaryVO);
+
+        for(MultipartFile file:files){
+            String url = storageUploader.uploadFile(file);
+
+            ImageVO image = ImageVO.builder()
+                    .memberId(memberId)
+                    .targetType(ImageTargetType.DIARY)
+                    .targetId(diaryVO.getDiaryId())
+                    .fileUrl(url)
+                    .fileName(file.getOriginalFilename())
+                    .build();
+            result += imageMapper.insertImage(image);
+
+        }
+        if(result == files.size()+1){
+            return result;
+        }
+        throw new IllegalStateException("관찰일지 등록 실패(업데이트 누락)");
+    }
+}
