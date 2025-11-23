1298e28430ec700b158ca80057618703cae5977f
KBH <galaxysgoo418@gmail.com>
2025-11-23 23:09:40 +0900

    feat: add report service test

--- src/main/java/com/zero/plantory/domain/report/mapper/ReportMapper.java
+++ src/main/java/com/zero/plantory/domain/report/mapper/ReportMapper.java
@@ -3,6 +3,7 @@ package com.zero.plantory.domain.report.mapper;
 import com.zero.plantory.global.vo.ReportVO;
 import com.zero.plantory.domain.report.vo.SelectNameListVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.List;
 @Mapper
 public interface ReportMapper {
     List<SelectNameListVO> selectUserIdByNickname(@Param("nickname") String nickname);
+    @Options(useGeneratedKeys = true, keyProperty = "reportId", keyColumn = "report_id")
     int insertReport(ReportVO reportVO);
 }
