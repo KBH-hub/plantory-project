3c3eb314c9aaaf02a28eac7155ba3011a4a8190d
kbh_kosta <galaxysgoo418@gmail.com>
2025-11-26 10:52:29 +0900

    refactor: reorganize html & js files

--- src/main/java/com/zero/plantory/global/cmdController/CmdController.java
+++ src/main/java/com/zero/plantory/global/cmdController/CmdController.java
@@ -1,11 +1,9 @@
 package com.zero.plantory.global.cmdController;
 
 import com.zero.plantory.domain.member.service.MemberService;
-import com.zero.plantory.global.vo.MemberVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
