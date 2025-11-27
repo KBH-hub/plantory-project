0b0b104975fb09d31f91ff0e5ee62ccdadff026f
kbh_kosta <galaxysgoo418@gmail.com>
2025-11-27 19:34:34 +0900

    refector: update myprofileTest name

--- src/test/java/com/zero/plantory/domain/member/service/MyProfileServiceImplTest.java
+++ src/test/java/com/zero/plantory/domain/member/service/MyProfileServiceImplTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class MyprofileServiceImplTest {
+class MyProfileServiceImplTest {
 
     @Mock
     private MyProfileMapper myProfileMapper;
