adefd477f691fba80269589a9162df015b2eab48
KBH <galaxysgoo418@gmail.com>
2025-11-30 03:44:17 +0900

    refactor: update watering method name

--- src/main/java/com/zero/plantory/domain/plantingCalendar/controller/PlantingCalenderRestController.java
+++ src/main/java/com/zero/plantory/domain/plantingCalendar/controller/PlantingCalenderRestController.java
@@ -49,7 +49,7 @@ public class PlantingCalenderRestController {
     }
 
     @DeleteMapping("/watering")
-    public ResponseEntity<Map<String, String>> deleteWateringFlag(@RequestParam Long myplantId, @RequestParam Long memberId) {
+    public ResponseEntity<Map<String, String>> deleteWatering(@RequestParam Long myplantId, @RequestParam Long memberId) {
         int result = plantingCalenderService.removePlantWatering(myplantId, memberId);
         if(result == 1) {
             return ResponseEntity.status(200).body(Map.of("message", "watering delete success"));
